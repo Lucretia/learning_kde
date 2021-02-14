b9f79823804ed452ab2a1eaf464e35151e0b9d16
Luke A. Guest <laguest@archeia.com>
2021-02-14 07:38:34 +0000

    Add Tutorial 1.

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <QApplication>
+#include <QCommandLineParser>
+#include <KAboutData>
+#include <KLocalizedString>
+#include <KMessageBox>
+
+int main (int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    KLocalizedString::setApplicationDomain("tutorials");
+
+    KAboutData aboutData(
+        // Internal program name.
+        QStringLiteral("tutorial1"),
+        // Dispplay name.
+        i18n("Tutorial 1"),
+        // Program's version string.
+        QStringLiteral("1.0"),
+        // Short description.
+        i18n("Displays a KMessageBox popup"),
+        // Licence.
+        KAboutLicense::GPL,
+        // Copyright.
+        i18n("(c) 2020"),
+        // Optional text shown.
+        i18n("Some text..."),
+        // Program's url.
+        QStringLiteral("http://example.org"),
+        // Bug report email.
+        QStringLiteral("bus@example.org"));
+
+    aboutData.addAuthor(
+        i18n("name"),
+        i18n("Task"),
+        QStringLiteral("user@example.org"),
+        QStringLiteral("http://example.org"),
+        QStringLiteral("OSC Username"));
+
+    KAboutData::setApplicationData(aboutData);
+
+    QCommandLineParser parser;
+
+    aboutData.setupCommandLine(&parser);
+    parser.process(app);
+    aboutData.processCommandLine(&parser);
+
+    KGuiItem yesButton(i18n("Hello"), QString(),
+        i18n("This is a tooltip"),
+        i18n("This is a WhatsThis help text."));
+
+    return KMessageBox::questionYesNo(nullptr,
+        i18n("Hello, world!"),
+        i18n("Hello"),
+        yesButton) == KMessageBox::Yes ? EXIT_SUCCESS : EXIT_FAILURE;
+}
